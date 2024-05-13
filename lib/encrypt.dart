1319b2c462c6cd8981de1dded73d15df05c2c0f2
7aGiven <fredrickareichert@gmail.com>
2024-05-13 12:39:24 +0800

    0.2.0

--- lib/encrypt.dart
+++ lib/encrypt.dart
@@ -1,1 +1,23 @@
 library encrypt;
+
+export 'src/aes.dart';
+export 'src/salsa20.dart';
+
+abstract class Algorithm {
+  String encrypt(String plainText);
+  String decrypt(String cipherText);
+}
+
+class Encrypter {
+  final Algorithm algo;
+
+  Encrypter(this.algo);
+
+  String encrypt(String plainText) {
+    return algo.encrypt(plainText);
+  }
+
+  String decrypt(String cipherText) {
+    return algo.decrypt(cipherText);
+  }
+}
